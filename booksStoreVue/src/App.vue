e903a060aab15da7577e627bce74ada61dd2933e
Ivan_Bartov <vanya.bartov@gmail.com>
2023-02-13 15:35:09 +0200

    initial commit

---
+++ booksStoreVue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="main" class="d-flex align-items-center justify-content-center">
+    <div>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+onMounted(() => {
+    router.push("/home");
+});
+
+</script>
+
+<style>
+#main {
+  min-height: 100vh;
+}
+</style>
---
+++ booksStoreVue/src/components/Home.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        sadsafdasfsa
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
